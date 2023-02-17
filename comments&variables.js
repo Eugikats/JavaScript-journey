6566b28732e9623874792c0ce85e0affc3812a90
Eugene Katusiime <katusiime66@gmail.com>
2023-02-17 11:51:02 +0300

    Added Operators and variables

---
+++ comments&variables.js
@@ -0,0 +1,9 @@
+// COMMENTS
+
+// Used to provide info about the code
+// Can be used to provide info or suggestions
+// Makes it easy to understand code or get more info
+// Can also be used to comment out code sections
+// They're ignored by the JS system
+// They can be single or multi line
+// For multi line comments, use /* at the beginning and end of your code */.
